cf6b26b098263cf685b0ed9bdb2315a593d42c82
alexmeier2828 <ameier42@gmail.com>
2025-08-29 07:32:35 -0400

    Added easymotion setup

---
+++ nvim/lua/plugins/easymotion.lua
@@ -0,0 +1,14 @@
+return {
+    'easymotion/vim-easymotion',
+    ---@module @vim-easymotion
+    
+    keys = {
+        {'<leader><leader>/', '<Plug>(easymotion-sn)', desc = 'Easy motion search'},
+        {'<leader><leader>g', '<Plug>(easymotion-overwin-f2)', desc = 'Easy motion overwin f2'},
+        {'<leader><leader>g', '<Plug>(easymotion-overwin-f2)', desc = 'Easy motion overwin f2'},
+        {'<leader>r', '<Plug>(easymotion-repeat)', desc = 'Easy motion repeat motion'},
+    },
+    lazy = false
+
+}
+
